682845b153cc9ed4a30e53a14e6a91710c553b18
grzwolf <grzwolf1@gmail.com>
2022-09-26 20:27:30 +0200

    cleanup import

--- app/src/main/java/com/grzwolf/grzlog/HelpActivity.kt
+++ app/src/main/java/com/grzwolf/grzlog/HelpActivity.kt
@@ -3,7 +3,6 @@ package com.grzwolf.grzlog
 import android.os.Bundle
 import android.view.*
 import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
