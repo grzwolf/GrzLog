478ea53d44bb23f698890fdb28cdbdc5e45cd47d
grzwolf <grzwolf1@gmail.com>
2023-01-31 18:41:36 +0100

    update help pages

--- app/src/main/java/com/grzwolf/grzlog/HelpActivity.kt
+++ app/src/main/java/com/grzwolf/grzlog/HelpActivity.kt
@@ -11,87 +11,111 @@ class HelpActivity : AppCompatActivity() {
     lateinit var textView: EditText
 
     val text =
-        "Help\n\n" +
-        "1. UI Elements\n" +
-        "==============\n" +
-        "1.1 ListView with items\n" +
-        "1.2 Red plus button\n" +
-        "1.3 Menubar with Hamburger icon\n" +
+        "1. User Interface Elements\n" +
+        "==========================\n" +
+        "1.1 Red + button\n" +
+        "1.2 ListView with items of current folder\n" +
+        "1.3 Menu bar with Hamburger icon\n" +
         "\n" +
-        "2. UI Actions\n" +
-        "=============\n" +
-        "2.1 ListView item\n" +
-        "~~~~~~~~~~~~~~~~~\n" +
-        "a) long press item -> current item input options dialog\n" +
-        "    edit item\n" +
-        "    copy item\n" +
-        "    insert new item before current item\n" +
-        "    insert new item after currentitem\n" +
-        "    select items dialog\n" +
-        "    use item as lockscreen reminder\n" +
-        "    delete item\n" +
-        "    ! all changes can be reverted by MenuBar Undo !\n" +
-        "b) click item center screen -> show 'foo [attachment] bar' \n" +
-        "c) click item left bound    -> select current item\n" +
-        "d) long press selection     -> select options dialog\n" +
-        "    unselect all items\n" +
-        "    select all items\n" +
-        "    invert items selection\n" +
-        "    select items of the day\n" +
-        "    copy selection\n" +
-        "    cut selection\n" +
-        "    delete selection\n" +
-        "\n" +
-        "2.2 Red plus button\n" +
+        "2. User Interface Actions\n" +
+        "=========================\n" +
+        "2.1 Red plus button\n" +
         "~~~~~~~~~~~~~~~~~~~\n" +
-        "a) click red plus button -> add new item dialog\n" +
-        "    insert text\n" +
-        "    add attachment -> attachment options dialog\n" +
-        "        image from phone gallery\n" +
-        "        all types file from GrzLog gallery\n" +
-        "        capture camera image\n" +
-        "        video from phone gallery\n" +
-        "        audio from phone\n" +
-        "        PDF file\n" +
-        "        TXT file\n" +
-        "b) long press red + button -> ListView jump\n" +
-        "    jump to top\n" +
-        "    jump to bottom\n" +
+        "a) tap red + button -> 'Write to GrzLog'\n" +
+        "   Edit: type or paste text\n" +
+        "         multi line is ok\n" +
+        "         weblinks are highlighted\n" +
+        "   Attachment: 'Select attachment' dialog\n" +
+        "         image from phone gallery\n" +
+        "         files from GrzLog gallery\n" +
+        "         capture camera image\n" +
+        "         video from phone gallery\n" +
+        "         audio from phone\n" +
+        "         PDF file\n" +
+        "         TXT file\n" +
+        "         WWW link\n" +
+        "         GrzLog folder\n" +
+        "   !revert latest change via Menu bar Undo!\n\n" +
+        "b) tap long red + button -> 'Jump to ...'\n" +
+        "        jump to top\n" +
+        "        jump to bottom\n" +
         "\n" +
-        "2.3 Menubar\n" +
-        "~~~~~~~~~~~\n" +
-        "a) click MenuBar Hamburger\n" +
-        "        more MenuBar icons appear\n" +
-        "b) click again Hamburger -> settings window\n" +
-        "        about\n" +
-        "        developer contact data\n" +
-        "        this help\n" +
-        "        input placement: new at top (like email) or at bottom (like messengers)\n" +
-        "        tap widget: show GrzLog or jump to input\n" +
-        "        preview image auto close\n" +
-        "        dark mode on or off\n" +
-        "        use GCam (if installed) instead of default one\n" +
-        "        backup data to /Download\n" +
-        "        restore data from /Download\n" +
-        "        import backup from explorer: Android 13 limits file access\n" +
-        "        show GrzLog file gallery\n" +
-        "        tidy orphaned files from GrzLog gallery\n" +
-        "        reset app preferences (data are not touched)\n" +
-        "c) click Share -> share options dialog\n" +
-        "        share current item\n" +
-        "        share surrounding day\n" +
-        "        share current folder\n" +
-        "        share current folder as PDF\n" +
-        "        share current folder as RTF\n" +
-        "d) click Folder -> folder options dialog\n" +
-        "        list of folder \n" +
-        "        more: Export to PDF, TXT, clipboard  \n" +
-        "        more: new folder\n" +
-        "        more: rename folder\n" +
-        "        more: clear folder content\n" +
-        "        more: remove/delete folder\n" +
-        "        more: move folder up in list\n" +
-        "        more: set input timestamp HH:MM, HH:MM:SS, none  \n" +
+        "2.2 ListView item\n" +
+        "~~~~~~~~~~~~~~~~~\n" +
+        "a) tap long item -> 'What to do ...' dialog\n" +
+        "       Edit line\n" +
+        "       Copy line\n" +
+        "       Insert line above\n" +
+        "       Insert line below\n" +
+        "       Select items dialog\n" +
+        "       Line as lockscreen reminder\n" +
+        "       Toggle line as header\n" +
+        "       Delete line\n" +
+        "   !revert latest change via Menu bar Undo!\n\n" +
+        "b) tap item somewhere  -> show item content\n\n" +
+        "c) double tap item     -> show item content\n\n" +
+        "d) tap item left bound -> toggle item selection\n\n" +
+        "e) tap long selection  -> 'What to do ...' dialog\n" +
+        "       Toggle item selection\n" +
+        "       Select next 10 items\n" +
+        "       Select items of day\n" +
+        "       Select items of month\n" +
+        "       Unselect all items\n" +
+        "       Select all items\n" +
+        "       Invert items selection\n" +
+        "       Copy selection\n" +
+        "       Cut selection\n" +
+        "       Delete selection\n" +
+        "   !revert latest change via Menu bar Undo!\n" +
+        "\n" +
+        "2.3 Menu bar\n" +
+        "~~~~~~~~~~~~\n" +
+        "a) tap Hamburger\n" +
+        "       more icons will appear: Loupe, Folder, Share\n" +
+        "       more icons disappear after 10s\n\n" +
+        "b) 2x tap Hamburger -> Settings window\n" +
+        "       About\n" +
+        "       Developer contact data\n" +
+        "       Help\n" +
+        "       Input placement: new at top (like email) or at bottom (like messengers)\n" +
+        "       Tap widget: show GrzLog or jump to input\n" +
+        "       Preview image auto close\n" +
+        "       Dark mode on or off\n" +
+        "       Use GCam (if installed) instead of default one\n" +
+        "       Backup outdated reminder\n" +
+        "       Backup data info\n" +
+        "       Backup data to /Download\n" +
+        "       Restore data from /Download\n" +
+        "       Restore from file explorer: Android 13 limits file access\n" +
+        "       Show GrzLog file gallery\n" +
+        "       Tidy orphaned files from GrzLog gallery\n" +
+        "       Reset app preferences (data are not touched)\n\n" +
+        "c) tap Loupe -> type search word + Enter\n" +
+        "       current folder will be searched\n" +
+        "       search hits are marked\n" +
+        "       use up/down symbols to jump\n" +
+        "       end search by tap on Loupe + 2x X\n\n" +
+        "d) tap Folder -> 'Select folder' dialog\n" +
+        "       list of folders \n" +
+        "       more: Export to PDF, TXT, clipboard  \n" +
+        "       more: New folder\n" +
+        "       more: Rename folder\n" +
+        "       more: Clear folder content\n" +
+        "       more: Remove/delete folder\n" +
+        "       more: Move folder up in list\n" +
+        "       more: Set input timestamp HH:MM, HH:MM:SS, none  \n" +
+        "       more: Search all folders\n" +
+        "             type a global search phrase\n" +
+        "             a list of search hits will appear\n" +
+        "             select & jump to hit\n" +
+        "             jump back to search hit list via red back button\n\n" +
+        "e) tap Share -> share options dialog\n" +
+        "       share selected items\n" +
+        "       share current folder\n" +
+        "       share current folder as PDF\n" +
+        "       share current folder as RTF\n\n" +
+        "f) Updates download from\n" +
+        "   https://github.com/grzwolf/GrzLog/tags\n" +
         "\n"
 
     override fun onStart() {
